7d332db9fbf8803f5f7b4f6a288b7deff693b853
Ore Richard Muyiwa <richardmcfriend@live.com>
2018-06-28 11:41:16 +0100

    Update license informations

--- system/functions/global.php
+++ system/functions/global.php
@@ -7,7 +7,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2017 Onehyr Technologies Limited
+ * Copyright (c) 2018 Ore Richard Muyiwa
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
