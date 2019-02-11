7a3afb2db4e02959d8e640b6b4cadbf79cf37ce4
Ore Richard Muyiwa <richardmcfriend@live.com>
2019-02-12 00:42:25 +0100

    Updated error reporting to include stack traces and look more frameworky.

--- system/functions/error.php
+++ system/functions/error.php
@@ -42,66 +42,154 @@ if(!defined('CANDY')){
 
 /**
  * Fires a Candy error.
- * @param $s
+ * @param $errno
+ * @param $errstr
+ * @param $errfile
+ * @param $errline
  */
-$candy_error = function ($s){
-
-    if(strtolower(get_config('show_error', 'main')) == 'no'){
-        return '';
+$candy_error = function($errno, $errstr='', $errfile='', $errline='')
+{
+    // if error has been supressed with an @
+    if (error_reporting() == 0) {
+        return;
+    } else if(strtolower(get_config('show_error', 'main')) == 'no'){
+        return;
     }
 
-    $gt = debug_backtrace();
+    $cfg = [];
 
-    if(!function_exists('gsf')){
+    // check if function has been called by an exception
+    if(func_num_args() == 5) {
+        // called by trigger_error()
+        $exception = null;
+        list($errno, $errstr, $errfile, $errline) = func_get_args();
 
-        function gsf($ft){
-            $ur = '';
-            $ur .= ("<br /><br />Backtrace (most recent calls):<br /><br />\n");
-            if(is_object($ft)){
-                return print_r($ft, true);
-            }
-            foreach($ft as &$bt)
-            {
-                if(!is_array($bt))
-                    $bt = print_r($bt, true);
-                if(!isset($bt["file"]))
-                    $ur .= ("[PHP core called function]<br />");
-                else
-                    $ur .= ("File: ".$bt["file"]."<br />");
-    
-                if(isset($bt["line"]))
-                    $ur .= ("&nbsp;&nbsp;&nbsp;&nbsp;line: ".$bt["line"]."<br />");
-                $ur .= ("&nbsp;&nbsp;&nbsp;&nbsp;function called: ".print_r(isset($bt["function"]) ? $bt["function"] : '', true));
-    
-                if(isset($bt["args"]))
-                {
-                    $ur .= ("<br />&nbsp;&nbsp;&nbsp;&nbsp;args: ");
-                    for($j = 0; $j <= count($bt["args"]) - 1; $j++)
-                    {
-                        if(is_array($bt["args"][$j]) || is_object($bt["args"][$j]))
-                        {
-                            $ur .= gsf($bt["args"][$j]) . '<br/>';
-                        }
-                        else {
-                            $ur .= print_r($bt["args"][$j], true) . '<br/>';
-                        }
-    
-                        if($j != count($bt["args"]) - 1)
-                            $ur .= ", ";
-                    }
+        $backtrace = array_reverse(debug_backtrace());
+
+    }else {
+        // caught exception
+        $exc = func_get_arg(0);
+        $errno = $exc->getCode();
+        $errstr = $exc->getMessage();
+        $errfile = $exc->getFile();
+        $errline = $exc->getLine();
+
+        $backtrace = $exc->getTrace();
+    }
+
+    $errorType = array (
+               E_ERROR            => 'ERROR',
+               E_WARNING        => 'WARNING',
+               E_PARSE          => 'PARSING ERROR',
+               E_NOTICE         => 'NOTICE',
+               E_CORE_ERROR     => 'CORE ERROR',
+               E_CORE_WARNING   => 'CORE WARNING',
+               E_COMPILE_ERROR  => 'COMPILE ERROR',
+               E_COMPILE_WARNING => 'COMPILE WARNING',
+               E_USER_ERROR     => 'USER ERROR',
+               E_USER_WARNING   => 'USER WARNING',
+               E_USER_NOTICE    => 'USER NOTICE',
+               E_STRICT         => 'STRICT NOTICE',
+               E_RECOVERABLE_ERROR  => 'RECOVERABLE ERROR'
+               );
+
+    // create error message
+    if (array_key_exists($errno, $errorType)) {
+        $err = $errorType[$errno];
+    } else {
+        $err = 'ERROR';
+    }
+
+    $errMsg = "$err: $errstr in $errfile on line $errline\n";
+
+	$trace = '';
+    // start backtrace
+	$i = 0;
+    foreach ($backtrace as $v) {
+		
+        if (isset($v['class'])) {
+
+		$trace .= "\n <strong>#{$i}</strong> Called Class: {$v['class']}::{$v['function']}(";
+
+            if (isset($v['args'])) {
+                $separator = '';
+
+                foreach($v['args'] as $arg ) {
+                    $trace .= "$separator".__get_error_arg($arg);
+                    $separator = ', ';
                 }
-                $ur .= ("<br /><br />");
-    
-                return $ur;
             }
-        };
+			$trace .= ")" .(isset($v['file']) ? "\n   File: {$v['file']}" : ''). (isset($v['line']) ? "\n   Line: {$v['line']}" : '');
+        }
+        elseif (isset($v['function'])) {
+            $trace .= "\n<strong>#{$i}</strong> Called Function: {$v['function']}(";
+            if (!empty($v['args'])) {
+
+                $separator = '';
+
+                foreach($v['args'] as $arg ) {
+                    $trace .= "$separator".__get_error_arg($arg);
+                    $separator = ', ';
+                }
+            }
+            $trace .= ")" .(isset($v['file']) ? "\n   File: {$v['file']}" : ''). (isset($v['line']) ? "\n   Line: {$v['line']}" : '');
+        }
+		$i++;
     }
 
-    $uq = gsf($gt);
+    // display error msg, if debug is enabled
+    $result = "$errMsg\n\n<strong style='font-size:1.25em'>Trace:</strong>\n{$trace}\n";
 
+    // what to do
+    switch ($errno) {
+        case E_NOTICE:
+        case E_USER_NOTICE:
+            return;
+            break;
 
-    die(CANDY_ERROR_PREPEND_STRING . $s . $uq . CANDY_ERROR_APPEND_STRING);
+        default:
+            die(CANDY_ERROR_PREPEND_STRING . $result . CANDY_ERROR_APPEND_STRING);
+            break;
+
+    }
+
 };
+
+function __get_error_arg($arg)
+{
+    switch (strtolower(gettype($arg))) {
+
+        case 'string':
+            return( '"'.preg_replace( '/\\n/', '', $arg ).'"' );
+
+        case 'boolean':
+            return (bool)$arg;
+
+        case 'object':
+            return "{".get_class($arg)."}";
+
+        case 'array':
+            $ret = "\n   [\n     ";
+            $separtor = "";
+
+            foreach ($arg as $k => $v) {
+                //$ret .= $separtor.__get_error_arg($k).' => '.__get_error_arg($v);
+				$g = @var_export($v, true);
+				$ret .= "{$separtor}\${$k} => ".preg_replace('/\n+/', "\n     ", strip_tags($g));
+                $separtor = ",\n     ";
+            }
+            $ret .= "\n   ]";
+
+            return $ret;
+
+        case 'resource':
+            return 'resource('.get_resource_type($arg).')';
+
+        default:
+            return var_export($arg, true);
+    }
+}
+
 
 /**
  * Fires an error when it exists in a model (creation, existence, mode and/or details).
